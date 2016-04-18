50ec83cf691636abea3573a2c0181f066b1a2d7a
guanhuan <yuanguanhuang@yy.com>
2016-04-18 10:25:12 +0800

    刪除app

--- app/src/main/java/oms/mmc/fortunetelling/fate/lib/model/entity/BaseResponse.java
+++
@@ -1,9 +0,0 @@
-package oms.mmc.fortunetelling.fate.lib.model.entity;
-
-/**
- * Created by Administrator on 2016/3/29.
- */
-public class BaseResponse {
-    public String status;
-    public String content;
-}
